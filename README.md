1b4d954381aee1b55d2c62bb36e417c73870377a
Your Name <you@example.com>
2021-12-29 11:48:00 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Paramedic-Assistant-Program
---
+++ tonterias.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+	
+char ansyn;
+
+do{
+	printf ("¿Cuento con ASC? Y/N  \n");
+	ansyn=getch();
+	} while (toupper (ansyn)=='N');
+do{
+printf ("Escena segura? Y/N  \n");
+	ansyn=getch();
+	} while (toupper (ansyn)=='N');
+return 0;
